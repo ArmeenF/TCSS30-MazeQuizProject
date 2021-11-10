eb853c59718c8324b03f01196138edd750b84827
Joseph Graves <jgrave@uw.edu>
2021-11-09 22:28:23 -0800

    Added Model Fields and Methods. Now has a constant for both the end node and the row length. Adjacency matrix gets created properly. PropertyChangeSupport properly notifies observers. canPlayerWin() functions. All tests pass.

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,60 +1,157 @@
 package model;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
+/**
+ * A model representing a trivia maze.
+ * @author Joseph Graves
+ * @version Fall 2021
+ */
 public class Model implements ModelInterface {
+
+    /**
+     * The node designated the end, and also the end index of the adjacency array.
+     */
+    public static final int END_NODE = 15;
+
+    /**
+     * The row length of the nodes, if they were to be placed in a square array.
+     */
+    public static final int ROW_LENGTH = (int) Math.sqrt(END_NODE + 1.0);
+
+    /**
+     * The adjacency matrix which shows valid moves.
+     */
+    private boolean[][] myAdjacencyMatrix;
+
+    /**
+     * The player's node position.
+     */
+    private int myPlayerPosition;
+
+    /**
+     * Property change support to help facilitate an MVC design.
+     */
+    private final PropertyChangeSupport myPropertyChangeSupport;
+
+    /**
+     * Create a model of a trivia maze.
+     */
+    public Model() {
+        myPropertyChangeSupport = new PropertyChangeSupport(this);
+        setUpNewGame();
+    }
+
     @Override
     public boolean[][] getAdjacencyMatrix() {
-        return new boolean[0][];
+        return myAdjacencyMatrix;
     }
 
     @Override
     public int getPlayerPosition() {
-        return 0;
+        return myPlayerPosition;
     }
 
     @Override
     public int getEndPosition() {
-        return 0;
+        return END_NODE;
     }
 
     @Override
     public boolean isPlayerAtEnd() {
-        return false;
+        return myPlayerPosition == END_NODE;
     }
 
     @Override
     public boolean canPlayerWin() {
-        return false;
+        /*
+         * This uses DFS to construct a list of nodes using
+         * the player's position as the start point.
+         * If END_NODE isn't on the list, the player cannot win.
+         */
+        List<Integer> nodeList = new ArrayList<>();
+        Deque<Integer> nodeDeque = new ArrayDeque<>();
+        int current = myPlayerPosition;
+        nodeList.add(current);
+        nodeDeque.add(current);
+        while (!nodeDeque.isEmpty()) {
+            for (int i = 0; i < END_NODE + 1; i++) {
+                if (myAdjacencyMatrix[current][i] && !nodeList.contains(i)) {
+                    nodeList.add(i);
+                    nodeDeque.add(i);
+                }
+            }
+            current = nodeDeque.pop();
+        }
+        return nodeList.contains(END_NODE);
     }
 
     @Override
-    public void setPlayerPosition(int thePosition) {
-
+    public void setPlayerPosition(final int thePosition) {
+        int oldValue = myPlayerPosition;
+        myPlayerPosition = thePosition;
+        myPropertyChangeSupport.firePropertyChange(ModelInterface.PLAYER_POSITION, oldValue, thePosition);
     }
 
     @Override
-    public void setVertice(int theRow, int theColumn, boolean theValue) {
-
+    public void setVertice(final int theRow, final int theColumn, final boolean theValue) {
+        boolean oldValue = myAdjacencyMatrix[theRow][theColumn];
+        myAdjacencyMatrix[theRow][theColumn] = theValue;
+        myPropertyChangeSupport.firePropertyChange(ModelInterface.ADJACENCY_MATRIX, oldValue, theValue);
     }
 
     @Override
-    public void addPropertyChangeListener(String thePropertyName, PropertyChangeListener theListener) {
-
+    public void addPropertyChangeListener(final String thePropertyName, final PropertyChangeListener theListener) {
+        myPropertyChangeSupport.addPropertyChangeListener(thePropertyName, theListener);
     }
 
     @Override
-    public void addPropertyChangeListener(PropertyChangeListener theListener) {
-
+    public void addPropertyChangeListener(final PropertyChangeListener theListener) {
+        myPropertyChangeSupport.addPropertyChangeListener(theListener);
     }
 
     @Override
-    public void removePropertyChangeListener(String thePropertyName, PropertyChangeListener theListener) {
-
+    public void removePropertyChangeListener(final String thePropertyName, final PropertyChangeListener theListener) {
+        myPropertyChangeSupport.removePropertyChangeListener(thePropertyName, theListener);
     }
 
     @Override
-    public void removePropertyChangeListener(PropertyChangeListener theListener) {
-
+    public void removePropertyChangeListener(final PropertyChangeListener theListener) {
+        myPropertyChangeSupport.removePropertyChangeListener(theListener);
     }
+
+    /**
+     * Sets a row column pair and it's mirrored value across the diagonal to a value.
+     * @param theRow the row.
+     * @param theColumn the column.
+     * @param theValue the value to set.
+     */
+    public void setSymmetricalVertice(final int theRow, final int theColumn, final boolean theValue) {
+        setVertice(theRow, theColumn, theValue);
+        setVertice(theColumn, theRow, theValue);
+    }
+
+    /**
+     * Sets up a new game. Sets player position to 0, and sets the appropriate vertex values in the
+     * adjacency matrix.
+     */
+    private void setUpNewGame() {
+        myPlayerPosition = 0;
+        myAdjacencyMatrix = new boolean[END_NODE + 1][END_NODE + 1];
+        for (int i = 0; i < END_NODE + 1; i++){
+            if ((i+1)%ROW_LENGTH != 0) {
+                setSymmetricalVertice(i, i + 1, true);
+            }
+            if (i + ROW_LENGTH < END_NODE + 1) {
+                setSymmetricalVertice(i, i+4, true);
+            }
+        }
+    }
+
+
 }
