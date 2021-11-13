f3dff7b73945414cea43e3ce72665672dd0d766a
Joseph Graves <jgrave@uw.edu>
2021-11-13 15:39:20 -0800

    Removed unused imports, merged

--- src/main/TriviaMazeFrame.java
+++ src/main/TriviaMazeFrame.java
@@ -4,11 +4,6 @@ import controller.Controller;
 import model.Model;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class TriviaMazeFrame extends JFrame {
 
